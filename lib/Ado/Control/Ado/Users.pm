7f8828e3fa9e5c8929c9ee6551984f2d4172c635
Krasimir Berov <k.berov@gmail.com>
2013-12-22 14:13:22 +0100

    Params::Check::check in ado-users#list action

--- lib/Ado/Control/Ado/Users.pm
+++ lib/Ado/Control/Ado/Users.pm
@@ -6,13 +6,30 @@ use Mojo::Base 'Ado::Control::Ado';
 sub list {
     my $c = shift;
     $c->require_formats(['json']) || return;
-    my @range = ($c->param('limit') || 10, $c->param('offset') || 0);
-    $c->res->headers->content_range("users $range[1]-${\($range[0] + $range[1])}/*");
-    $c->debug("rendering json only [@range]");
+    my $args = Params::Check::check(
+        {   limit => {
+                allow => sub { $_[0] =~ /^\d+$/ ? 1 : ($_[0] = 20); }
+            },
+            offset => {
+                allow => sub { $_[0] =~ /^\d+$/ ? 1 : defined($_[0] = 0); }
+            },
+        },
+        {   limit  => $c->req->param('limit')  || 20,
+            offset => $c->req->param('offset') || 0,
+        }
+    );
+
+    $c->res->headers->content_range(
+        "users $$args{offset}-${\($$args{limit} + $$args{offset})}/*");
+    $c->debug("rendering json only [$$args{limit}, $$args{offset}]");
 
     #content negotiation
     return $c->respond_to(
-        json => $c->list_for_json(\@range, [Ado::Model::Users->select_range(@range)]));
+        json => $c->list_for_json(
+            [$$args{limit}, $$args{offset}],
+            [Ado::Model::Users->select_range($$args{limit}, $$args{offset})]
+        )
+    );
 }
 
 sub add {
