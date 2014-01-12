86bee99409cef936e8c926423e389cf8b729862e
Krasimir Berov <k.berov@gmail.com>
2014-01-12 23:48:09 +0100

    default accessors

--- lib/Ado/Sessions.pm
+++ lib/Ado/Sessions.pm
@@ -1,5 +1,12 @@
 package Ado::Sessions;
 use Mojo::Base -base;
+use Mojo::JSON;
+use Mojo::Util qw(b64_decode b64_encode);
+
+has [qw(cookie_domain secure)];
+has cookie_name        => 'ado';
+has cookie_path        => '/';
+has default_expiration => 3600;
 
 sub generate_id {
     return Mojo::Util::sha1_hex(rand() . $$ . {} . time);
