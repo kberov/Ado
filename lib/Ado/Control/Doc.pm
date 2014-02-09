51304eef0c3622cafa7a619a6fa3c8e09aff1875
Krasimir Berov <k.berov@gmail.com>
2014-02-09 01:32:59 +0100

    saving state 3

---
+++ lib/Ado/Control/Doc.pm
@@ -0,0 +1,72 @@
+package Ado::Control::Doc;
+use Mojo::Base 'Ado::Control';
+
+#Renders the page found in md_file
+sub show {
+    my $c = shift;
+
+    return $c->render();
+}
+
+1;
+
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Ado::Control::Doc - The controller for the end-user documentation 
+
+=head1 SYNOPSIS
+
+  #in your browser go to
+  http://your-host/doc
+
+=head1 DESCRIPTION
+
+This is a minimal controller to display and browse end-user documentation
+written in markdown format.
+
+=head1 METHODS/ACTIONS
+
+L<Ado::Control::Doc> inherits all the methods from 
+L<Ado::Control> and defines the following ones.
+
+=head2 show
+
+Renders the page found in C<$c-E<gt>stash('md_file');>.
+
+=head1 SPONSORS
+
+The original author
+
+Become a sponsor and help make L<Ado> the ERP for the enterprise!
+
+=head1 SEE ALSO
+
+L<Ado::Plugin::MarkdownRenderer>,
+L<Ado::Control>, L<Mojolicious::Controller>, 
+L<Mojolicious::Guides::Growing/Model_View_Controller>,
+L<Mojolicious::Guides::Growing/Controller_class>
+
+
+=head1 AUTHOR
+
+Красимир Беров (Krasimir Berov)
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 Красимир Беров (Krasimir Berov).
+
+This program is free software, you can redistribute it and/or
+modify it under the terms of the 
+GNU Lesser General Public License v3 (LGPL-3.0).
+You may copy, distribute and modify the software provided that 
+modifications are open source. However, software that includes 
+the license may release under a different license.
+
+See http://opensource.org/licenses/lgpl-3.0.html for more information.
+
+=cut
--- lib/Ado/Plugin/MarkdownRenderer.pm
+++ lib/Ado/Plugin/MarkdownRenderer.pm
@@ -20,7 +20,8 @@ sub register {
 
 sub md_to_html {
     my ($c, $config) = @_;
-
+    my $file_path = $c->stash('md_file');
+    $c->debug("md_file: $file_path");
     my $md_renderer = $config->{md_renderer};
     my $e           = Mojo::Loader->load($md_renderer);
     if (ref $e) {
@@ -54,15 +55,37 @@ Ado::Plugin::MarkDownRenderer - Render static files in markdown format
 
 =head1 SYNOPSIS
 
-  #Open $MOJO_HOME/etc/plugins/markdown-renderer.conf and describe your routes
+  #1. Use one or more markup parsers
+  plugins => [
+    #...
+    'markdown_renderer',
+    {name => 'markdown_renderer', config => {
+      md_renderer =>'Text::Markup',
+      md_options => {
+        trac_url      => '/wiki',
+        disable_links => [ qw( changeset ticket ) ],
+      },
+      md_method => 'parse',
+      md_helper =>'trac_to_html'
+    }},
+    #...
+  ],
+
+  #2. Describe your routes
   routes     => [
-        {route => '/doc/*md_file', via => ['GET'],  
-          to => 'doc#show',},
-        #your own great enterprise wiki
+        #markdown_renderer route is already configured
+        #in etc/plugins/markdown_renderer.conf.
+        
+        #Your own great enterprise wiki
         {route => '/wiki/*md_file', via => ['GET'], 
           to => 'wiki#show',},
         #...
         ],
+  
+  #3. Write your controller
+  package Ado::Control::Wiki;
+  use Mojo::Base 'Ado::Control';
+  #...
 
 =head1 DESCRIPTION
 
@@ -70,29 +93,53 @@ L<Ado::Plugin::MarkdownRenderer> is a markdown renderer, rawr!
 
 You only need to create a controller for your enterprise wiki and use
 the L</md_to_html> helper provided by this plugin. 
-See Ado::Control::Doc for an example.
+See L<Ado::Control::Doc> for an example.
+
+You may use this plugin to load and use other markup languages parsers 
+and converters to HTML.
 
 The code of this plugin is a good example for learning to build new plugins,
 you're welcome to fork it.
 
 =head1 OPTIONS
 
+The following options can be set in C<etc/ado.conf>.
+You can find default options in C<etc/plugins/markdown_renderer.conf>.
+C<md> prefix is short for "markup document" or "markdown".
+
 =head2 md_renderer
 
   md_renderer => 'Text::MultiMarkdown',
 
-  The class to load. This option exists because hte use may want to use
-  L<Text::Markdown> or L<Text::Markup> instead.
+The class to load. This option exists because the user may want to use
+L<Text::Markdown> or L<Text::Markup> instead.
 
 =head2  md_method
 
   md_method => 'markdown',
 
 Method that will be used internally to produce the C<$HTML>.
-Can also be a code referrence.
-First paramether is the md_renderer instance and the 
+Can also be a code reference.
+First parameter is the md_renderer instance and the 
 second is the markdown text.
 
+=head2 md_options
+
+  md_options => {
+      use_wikilinks => 1,
+      base_url      => '/doc/bg/',
+  },
+
+These options will be passed to the md_renderer constructor. 
+They are specific for each markup parser so look at it's documentation.
+
+=head2 md_helper
+
+  md_helper => 'md_to_html',
+
+Default helper name. You may want to change this if you want to have
+different helpers for different markup converters,
+configurations, applications etc...
 
 =head1 HELPERS
 
@@ -102,7 +149,8 @@ L<Ado::Control> methods and templates.
 =head2 md_to_html
 
 Given a Markdown string returns the HTML produced by the renderer - 
-L<Text::Markdown> by default.
+L<Text::MultiMarkdown> by default. 
+You may want to use your own helper name. See L</md_helper>.
 
   #Markdown from $MOJO_HOME/public/doc/bg/intro.md
   #http://example.com/doc/bg/intro.md
@@ -120,13 +168,33 @@ L<Ado::Plugin> and implements the following new ones.
 
 =head2 register
 
-  my $route = $plugin->register(Ado->new);
-  my $route = $plugin->register(Ado->new, {options => {...}});
+Register renderer and helper in L<Ado>.
 
-Register renderer and helper in L<Mojolicious> application.
+=head1 SPONSORS
+
+The original author
+
+Become a sponsor and help make L<Ado> the ERP for the enterprise!
 
 =head1 SEE ALSO
 
-L<Ado::Plugin>, L<Ado::Manual>.
+L<Ado::Control::Doc>, L<Ado::Plugin>, L<Ado::Manual>.
+
+=head1 AUTHOR
+
+Красимир Беров (Krasimir Berov)
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2013 Красимир Беров (Krasimir Berov).
+
+This program is free software, you can redistribute it and/or
+modify it under the terms of the 
+GNU Lesser General Public License v3 (LGPL-3.0).
+You may copy, distribute and modify the software provided that 
+modifications are open source. However, software that includes 
+the license may release under a different license.
+
+See http://opensource.org/licenses/lgpl-3.0.html for more information.
 
 =cut
