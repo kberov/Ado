9911dcdc1e02b8bdacac035943ca583337920a05
Krasimir Berov <k.berov@gmail.com>
2014-05-11 02:35:54 +0200

    cleanup before real testing

--- lib/Ado/Plugin/I18n.pm
+++ lib/Ado/Plugin/I18n.pm
@@ -4,12 +4,27 @@ use I18N::LangTags qw(implicate_supers);
 use I18N::LangTags::Detect;
 use List::Util qw(first);
 use Time::Seconds;
-has routes => sub {
-    [
-#Any language prefixed front-end controllers
+
+#example.com/cz/foo/bar
+sub routes {
+    my $self   = shift;
+    my $config = $self->config;
+    return $$config{routes} if $$config{routes};
+    return [] unless $$config{language_from_route};
+    my $l = join '|', @{$$config{languages}};
+    return $self->config->{routes} = [
+
+#Language prefixed front-end controllers
+        {   route  => '/:language',
+            params => {$$config{language_param} => qr/(?:$l)/},
+            via    => [qw(GET OPTIONS)],
+
+            #Ado::Control::Default::index()
+            to => 'default#index',
+        },
         {   route  => '/:language/:controller',
             via    => [qw(GET OPTIONS)],
-            params => {language => [$_[0]->config('languages')]},
+            params => {$$config{language_param} => qr/(?:$l)/},
             to     => {
 
                 #Ado::Control::Default
@@ -19,7 +34,7 @@ has routes => sub {
         },
         {   route  => '/:language/:controller/:action',
             via    => [qw(GET POST OPTIONS)],
-            params => {language => [$_[0]->config('languages')]},
+            params => {$$config{language_param} => qr/(?:$l)/},
             to     => {
 
                 #Ado::Control::Default
@@ -29,7 +44,7 @@ has routes => sub {
         },
         {   route  => '/:language/:controller/:action/:id',
             via    => [qw(GET PUT DELETE OPTIONS)],
-            params => {language => [$_[0]->config('languages')]},
+            params => {$$config{language_param} => qr/($l)/},
             to     => {
 
                 #Ado::Control::Default
@@ -38,7 +53,8 @@ has routes => sub {
             }
         },
     ];
-};
+}
+
 
 sub register {
     my ($self, $app, $config) = @_;
@@ -56,9 +72,9 @@ sub register {
     $config->{languages} ||= ['en', 'bg'];
 
     #Try to get language from these places in the order below
-    $config->{language_from_param}   ||= 1;
+    $config->{language_from_route}   ||= 1;
     $config->{language_from_host}    ||= 1;
-    $config->{language_from_url}     ||= 1;
+    $config->{language_from_param}   ||= 1;
     $config->{language_from_cookie}  ||= 1;
     $config->{language_from_headers} ||= 1;
     $config->{language_param}        ||= 'language';
@@ -66,84 +82,108 @@ sub register {
     #Allow other namespaces too
     $config->{namespace} ||= 'Ado::I18n';
 
-    #eval "require $config->{namespace};";
     my $e = Mojo::Loader->new->load($config->{namespace});
-    warn qq{Loading "$config->{namespace}" failed: $e} if $e;
+    $app->log->warn(qq{Loading "$config->{namespace}" failed: $e}) if $e;
 
     #Add helpers
-    $app->helper(language => sub { &_language($config, @_) });
-    $app->helper(l        => \&_maketext);
-    $app->helper(maketext => \&_maketext);
+    $app->helper(
+        language => sub {
+            Ado::Plugin::I18n::language($config, @_);
+        }
+    );
+    $app->helper(l => \&_maketext);
 
-    #default routes including language tag.
+    #default routes including language placeholder.
     $app->load_routes($self->routes);
-    $app->load_routes($config->{routes}) if (@{$config->{routes} // []});
 
     # Add hook
-    $app->hook(before_routes => sub { _language($config, shift) });
+    $app->hook(
+        around_action => sub {
+
+            #batman:  put "local $_;" inside your callback before doing anything else
+            local $_ = undef;
+            my ($next, $c, $action, $last_step) = @_;
+            Ado::Plugin::I18n::language($config, $c);
+            return $next->();
+        }
+    );
     return $self;
 }
 
 #sets *once* and/or returns the current language - a controller property
-sub _language {
+sub language {
     my ($config, $c, $language) = @_;
+    state $l_param = $$config{language_param};
+    my $stash = $c->stash;
 
-    #already set? language()
-    $c->{language} && return $c->{language};
-    my $language_param = $config->{language_param};
 
-    #language('fr')
+    #language('fr') should be used in very
+    #rare cases since it is called in around_action
     if ($language) {
-        $c->{language} = $language;
+        $stash->{i18n} =
+          $$config{namespace}->get_handle($language, @{$$config{languages}});
+        return $stash->{$l_param} = $language;
     }
 
-    #?language=de
-    elsif ($$config{language_from_param}) {
-        $c->{language} = $c->param($language_param);
+    #already set? from route?
+    if ($stash->{$l_param}) {
+        $stash->{i18n} =
+          $$config{namespace}->get_handle($stash->{$l_param}, @{$$config{languages}});
+
+        $c->debug("already set \$stash->{$l_param}:" . $stash->{$l_param});
+        return $stash->{$l_param};
     }
 
     #bg.example.com
-    if (!$c->{language} && $config->{language_from_host}) {
-        ($c->{language}) = $c->req->headers->host =~ /^($_)\./;
+    if ($$config{language_from_host}
+        && (my ($l) = $c->req->headers->host =~ /^(\w{2})\./))
+    {
+        $stash->{$l_param} = $l;
+        $c->debug("language_from_host:$stash->{$l_param}");
     }
 
-    #example.com/cz/foo/bar
-    if (  !$c->{language}
-        && $config->{language_from_url}
-        && ($language = $c->req->url->path->parts->[0]))
-    {
-        $c->{language} = $language;
+    #?language=de
+    if (!$stash->{$l_param} && $$config{language_from_param}) {
+        $stash->{$l_param} = $c->param($l_param) // '';
+        $c->debug("language_from_param:\$stash->{$l_param}");
     }
-    if (  !$c->{language}
-        && $config->{language_from_cookie}
-        && ($language = $c->cookie($language_param)))
+
+
+    if (  !$stash->{language}
+        && $$config{language_from_cookie}
+        && ($language = $c->cookie($$config{language_param})))
     {
-        $c->cookie($language_param => $language, {expires => time + ONE_MONTH});
-        $c->{language} = $language;
+        $c->cookie($$config{language_param} => $language, {expires => time + ONE_MONTH});
+        $stash->{$l_param} = $language;
+        $c->debug("language_from_cookie:$stash->{$l_param}");
     }
 
     #Accept-Language:"bg,fr;q=0.8,en-us;q=0.5,en;q=0.3"
-    elsif (!$c->{language} && $config->{language_from_headers}) {
+    elsif (!$stash->{language} && $$config{language_from_headers}) {
         my @languages =
           I18N::LangTags::implicate_supers(
             I18N::LangTags::Detect->http_accept_langs($c->req->headers->accept_language));
         foreach my $language (@languages) {
-            $c->{language} = first { $_ =~ /$language/ } @{$config->{languages}};
-            last if $c->{language};
+            $stash->{$l_param} = first { $_ =~ /$language/ } @{$$config{languages}};
+            last if $stash->{$l_param};
         }
+        $c->debug("language_from_headers:$stash->{$l_param}");
     }
 
     #default
-    $c->{language} = $config->{default_language} unless $c->{language};
-    $c->{i18n} = $config->{namespace}->get_handle($c->{language}, @{$config->{languages}});
-    return $c->{language};
+    $stash->{$l_param} = $$config{default_language} unless $stash->{$l_param};
+
+
+    $stash->{i18n}
+      ||= $$config{namespace}->get_handle($stash->{$l_param}, @{$$config{languages}});
+    return $stash->{$l_param};
 }
 
 sub _maketext {
     my $c   = shift;
     my $key = shift;
-    return $key unless my $handle = $c->{i18n};
-    return $handle->maketext($key, @_);
+    return $key unless my $handle = $c->stash->{i18n};
+    return $handle->maketext($key ? ($key, @_) : '"maketext" requires at least one parameter.');
 }
 
 1;
@@ -156,6 +196,239 @@ sub _maketext {
 
 Ado::Plugin::I18n - Internationalization and localization for Ado
 
+=head1 SYNOPSIS
 
+This plugin just works. Nothing special needs to be done.
+
+    #Override the current language.
+    #you need to do this only in rare cases (like in an Ado::Command)
+    $c->language('bg');
+    #what is my language?
+    my $current_language = $c->language;
+
+=head1 DESCRIPTION
+
+L<Ado::Plugin::I18n> localize your application and site.
+It altomatically detects the current UserAgent language preferences
+and selects the best fit from the supported by the application languages.
+The current language is deteted and set in L<Mojolicious/around_action> hook.
+Various methods for setting the language are supported.
+
+=head1 OPTIONS
+
+The following options can be set in C<etc/ado.conf> or in C<etc/plugins/i18n.conf>.
+You have to create first C<etc/plugins/i18n.conf> so Ado can pick it up.
+You can enable all supported methods to detect the language in your application.
+
+The methods which will be used to detect and set the current
+language are as follows:
+
+  1. language_from_route,   eg. /bg/controller/action
+  2. language_from_host,    eg. fr.example.com
+  3. language_from_param,   eg. ?language=de
+  4. language_from_cookie,  eg. Cookie: language=bg;
+  5. language_from_headers, eg. Accept-Language: bg,fr;q=0.8,en-us;q=0.5,en;q=0.3
+
+Just be careful not to try to set the language in one request using two different
+methods eg. C</bg/controller/action?language=de>. 
+
+
+=head2 default_language
+
+The default value is C<en>. This language is used when Ado is not able to detect
+the language using any of the methods enabled by the options below.
+If you want to set a different language be sure to create a language class
+in your languages namespace. See also L</namespace>.
+
+=head2 language_from_route
+
+    {
+        language_from_route => 1
+        ...
+    }
+
+This is the first option that will be checked if enabled.
+The plugin prepares a default set of routes containing information 
+about the language.
+
+  /:language                          GET,OPTIONS
+  /:language/:controller              GET,OPTIONS
+  /:language/:controller/:action      GET,POST,OPTIONS
+  /:language/:controller/:action/:id  GET,PUT,DELETE,OPTIONS
+
+The language will be detected from current routes eg. C</bg/news/read/1234>
+and put into the stash. Default value is 1.
+
+=head2 language_from_host
+
+    {
+        language_from_host => 1,
+        language_from_route => 1,
+        ...
+    }
+
+This is the second option that will be checked if enabled.
+If you use languages as subdomains make sure to disable  C<language_from_route>
+or do not construct routes containing languages (eg. C<fr.example.com/en>).
+Default value is 1.
+
+=head2 language_from_param
+
+    {
+        
+        language_from_param => 1,
+        language_from_host =>  0,
+        language_from_route => 0,
+        ...
+    }
+
+This is the third option that will be checked if enabled and if the language is
+not yet detected using some of the previous methods.
+Make sure to not construct urls like C<fr.example.com?language=de>
+or even C<fr.example.com/bg?language=de>. The result is usually not what you want.
+Default value is 1.
+
+=head2 language_from_cookie
+
+    {
+        
+        language_from_cookie => 1,
+        language_from_param =>  1,
+        language_from_host =>   0,
+        language_from_route =>  0,
+        ...
+    }
+
+This is the fourth option that will be checked if enabled and if the language is
+not yet detected using some of the previous methods.
+This option is most suitable for applications which expect to find a cookie
+with name "language" and value one of the supported languages.
+Default value is 1.
+
+=head2 language_from_headers
+
+    {
+        
+        language_from_headers => 1
+        language_from_cookie  => 1,
+        language_from_param   => 1,
+        language_from_host    => 0,
+        language_from_route   => 0,
+        ...
+    }
+
+This is the fifth option that will be checked if enabled and if the language is
+not yet detected using some of the previous methods.
+It is best to keep this option enabled.
+Default value is 1.
+
+=head2 language_param
+
+    #language_param=> 'l'
+    current language is <%= $l %>
+    Cookie: l=bg;
+    #language_param=> 'lang'
+    current language is <%= $lang %>
+    Cookie: lang=bg;
+
+The name of the parameter(key) used in C<language_from_param>, C<language_from_route>
+and C<language_from_cookie>. this is also the key used in the C<$c-E<gt>stash>.
+Default value is "language".
+
+=head2 namespace
+
+The namespace used for language classes.
+Default value is L<Ado::I18n>.
+You rarely will want to change this.
+
+=head1 HELPERS
+
+L<Ado::Plugin::I18n> exports the following helpers for use in  
+L<Ado::Control> methods and templates.
+
+=head2 language
+
+Allows you to reset the current language. You do not need to use this helper
+it is called automatically in L<Mojolicious/around_action> hook.
+
+=head2 l
+
+Same as L<Locale::Maketext/maketext>.
+
+  $c->render(text => $c->l('hello', $c->user->name));
+  <%= l('hello', user->name) %>
+
+=head1 METHODS
+
+L<Ado::Plugin::I18n> inherits all methods from
+L<Ado::Plugin> and implements the following new ones.
+
+
+=head2 register
+
+This method is called by C<$app-E<gt>plugin>.
+Registers the plugin in L<Ado> application and merges internationalization
+and localization configuration from C<$MOJO_HOME/etc/ado.conf> with settings 
+defined in C<$MOJO_HOME/etc/plugins/i18n.conf>. Authentication settings 
+defined in C<ado.conf> will overwrite those defined in C<plugins/i18n.conf>.
+Returns C<$self>.
+
+=head2 routes
+
+Returns a list of routes with C<:language> placeholder
+defined in the plugin configuration. Called in L</register>.
+
+  #default routes including language placeholder.
+  $app->load_routes($self->routes);
+
+  /:language                          GET,OPTIONS
+  /:language/:controller              GET,OPTIONS
+  /:language/:controller/:action      GET,POST,OPTIONS
+  /:language/:controller/:action/:id  GET,PUT,DELETE,OPTIONS
+
+=head2 language
+
+This is the underlying subroutine used in C<around_action> hook and c<language>
+helper.
+
+    #Add helpers
+    $app->helper(
+        language => sub {
+            Ado::Plugin::I18n::language($config, @_);
+        }
+    );
+
+
+=head1 TODO
+
+Create a table with message entries which will be loaded by this plugin.
+Create user interface to add/edit entries.
+
+=head1 SEE ALSO
+
+L<Locale::Maketext>, L<Ado::Plugin>, L<Ado::Manual::Plugins>, 
+L<Mojolicious::Plugins>, 
+L<Mojolicious::Plugin>, L<Mojolicious::Guides::Routing/Conditions>
+
+=head1 SPONSORS
+
+The original author
+
+=head1 AUTHOR
+
+Красимир Беров (Krasimir Berov)
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2014 Красимир Беров (Krasimir Berov).
+
+This program is free software, you can redistribute it and/or
+modify it under the terms of the 
+GNU Lesser General Public License v3 (LGPL-3.0).
+You may copy, distribute and modify the software provided that 
+modifications are open source. However, software that includes 
+the license may release under a different license.
+
+See http://opensource.org/licenses/lgpl-3.0.html for more information.
 
 =cut
