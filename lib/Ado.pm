e7c94cfd69b08a322d388477e15aaf2003198a9a
Krasimir Berov <k.berov@gmail.com>
2014-05-15 22:34:04 +0200

    starting 0.42

--- lib/Ado.pm
+++ lib/Ado.pm
@@ -11,7 +11,7 @@ BEGIN {
     }
 }
 our $AUTHORITY = 'cpan:BEROV';
-our $VERSION   = '0.41';
+our $VERSION   = '0.42';
 our $CODENAME  = 'U+2C02 Главна буква ВЕДИ от Глаголицата (Ⰲ)';
 
 use Ado::Control;
