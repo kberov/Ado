eb0fdd86e97d8237e881de4f221749f840df8b86
Krasimir Berov <k.berov@gmail.com>
2014-07-22 01:36:10 +0200

    generate::adoplugin

---
+++ lib/Ado/Command/generate/adoplugin.pm
@@ -0,0 +1,26 @@
+package Ado::Command::generate::adoplugin;
+use Mojo::Base 'Ado::Command::generate';
+use Getopt::Long qw(GetOptionsFromArray :config no_auto_abbrev no_ignore_case);
+
+
+1;
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Ado::Command::generate::adoplugin - Generates an Ado::Plugin
+
+=head1 SYNOPSIS
+
+
+
+
+
+=head1 SEE ALSO
+
+
+
+=cut
