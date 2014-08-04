bf4b1b0c0deda085a0c7a4f1612a37e9013ae80f
Krasimir Berov <k.berov@gmail.com>
2014-08-04 20:30:00 +0200

    Bump version and improved documentation.

--- lib/Ado.pm
+++ lib/Ado.pm
@@ -11,7 +11,7 @@ BEGIN {
     }
 }
 our $AUTHORITY = 'cpan:BEROV';
-our $VERSION   = '0.55';
+our $VERSION   = '0.56';
 our $CODENAME  = 'U+2C04 GLAGOLITIC CAPITAL LETTER DOBRO (Ⰴ)';
 
 use Ado::Control;
