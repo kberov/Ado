296c3b01687cb0323e56a7b6eaf274bb1c3255a5
Krasimir Berov <k.berov@gmail.com>
2015-01-21 00:43:00 +0100

    up to 0.77 + Latest Changes

--- lib/Ado.pm
+++ lib/Ado.pm
@@ -11,7 +11,7 @@ BEGIN {
     }
 }
 our $AUTHORITY = 'cpan:BEROV';
-our $VERSION   = '0.76';
+our $VERSION   = '0.77';
 our $CODENAME  = 'U+2C07 GLAGOLITIC CAPITAL LETTER DZELO (Ⰷ)';
 
 use Ado::Control;
